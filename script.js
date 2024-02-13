19977f96d78e168610a20a5f5a8b425e21d62d1a
Vurdalack <chyngyz887@gmail.com>
2024-02-13 16:45:56 +0600

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+let nums = 0
+ const array = [100,55,20]
+  for(let i = nums; i < array.length; i ++){
+
+
+     console.log(`${nums} + ${array[i]} = ${nums + array[i]}`);
+     nums += array[i]
+  }
+
+
+
+  console.log('ответ чисел:',nums);
+
